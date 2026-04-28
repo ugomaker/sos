f20dd0c8e9578c5ccfb33d8f20883d9a389d021b
ugomaker <ugolino@live.fr>
2026-04-28 13:38:44 +0200

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,26 @@
+const CACHE = 'sos-urgence-v4';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE).then(function(cache) { cache.put(e.request, clone); });
+      return response;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
